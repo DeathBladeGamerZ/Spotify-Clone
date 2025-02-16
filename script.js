1d516873e1d6b397333352d1da435624b694aee0
DeathBladeGamerZ <166324810+DeathBladeGamerZ@users.noreply.github.com>
2025-02-16 23:21:04 +0530

    updated

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("js");
+async function FetchSongs() {
+  let a = await fetch("http://127.0.0.1:5500/Songs/");
+  let response = await a.text();
+  console.log(response);
+  let div = document.createElement("div");
+  div.innerHTML = response;
+  let as = div.getElementsByTagName("a");
+  let songs = [];
+  for (let i = 0; i < as.length; i++) {
+    const element = as[i];
+    if (element.href.endsWith(".mp3")) {
+      songs.push(element.href);
+    }
+  }
+   return songs
+}
+async function main() { 
+  let songs = await FetchSongs();
+  console.log(songs);
+
+  var audio = new Audio(songs[0]);
+  audio.play();
+}
+main(); 
