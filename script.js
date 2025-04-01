677734937a9b18c86e1ed24e46fc2bb7088a6179
DeathBladeGamerZ <166324810+DeathBladeGamerZ@users.noreply.github.com>
2025-04-01 22:32:17 +0530

    beta

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-console.log("js");
+let currentSong = new Audio();
 async function FetchSongs() {
   let a = await fetch("http://127.0.0.1:5500/Songs/");
   let response = await a.text();
-  console.log(response);
   let div = document.createElement("div");
   div.innerHTML = response;
   let as = div.getElementsByTagName("a");
@@ -10,16 +9,77 @@ async function FetchSongs() {
   for (let i = 0; i < as.length; i++) {
     const element = as[i];
     if (element.href.endsWith(".mp3")) {
-      songs.push(element.href);
+      songs.push(element.href.split("/Songs/")[1]);
     }
   }
    return songs
 }
-async function main() { 
-  let songs = await FetchSongs();
-  console.log(songs);
 
-  var audio = new Audio(songs[0]);
-  audio.play();
+const playMusic= (track, pause = false)=>{
+  // let audio = new Audio("/Songs/"+track)
+  currentSong.src = "/Songs/"+track;
+  if(!pause){
+    currentSong.play();
+    play.src = "Assets/pausebtn.svg";
+  }
+  
+  
+  sname.innerHTML=decodeURI(track) ;
 }
-main(); 
+
+function convertSeconds(seconds) {
+  let minutes = Math.floor(seconds / 60);
+  let remainingSeconds = seconds % 60;
+  return `${minutes.toString().padStart(2, '0')}:${Math.floor(remainingSeconds).toString().padStart(2, '0')}`;
+}
+
+
+async function main() { 
+  
+  let songs = await FetchSongs();
+  playMusic(songs[0],true);
+
+  // var audio = new Audio(songs[0]);
+  // audio.play();
+
+  // audio.addEventListener("loadeddata",()=>{
+  //   let duration = audio.duration; 
+  //   console.log(duration);
+  // })
+
+  let songUl = document.querySelector(".slist").getElementsByTagName("ul")[0]
+  for(const song of songs){
+    songUl.innerHTML = songUl.innerHTML + `<li>
+                <div class="songdetail">
+                  <img src="Assets/musicicon.svg" />
+                  <div class="sname">
+                    <p>${song.replaceAll("%20"," ")}</p>
+                    <p>Artist Name</p>
+                  </div>
+                  <img src="Assets/play-button-svgrepo-com.svg" />
+                </div>
+              </li>`;
+  }
+  Array.from(document.querySelector(".slist").getElementsByTagName("li")).forEach(e=>{
+    e.addEventListener("click",element=>{
+      console.log(e.querySelector(".sname>p"))
+       playMusic(e.querySelector(".sname>p").innerHTML)
+    })
+  })
+  play.addEventListener("click",()=>{
+    if(currentSong.paused){
+      currentSong.play();
+      play.src = "Assets/pausebtn.svg";
+    }
+    else{
+      currentSong.pause()
+      play.src = "Assets/play-btn.svg";
+    }
+  })
+  currentSong.addEventListener("timeupdate",()=>{
+    currtime.innerHTML = convertSeconds(currentSong.currentTime);
+    ttime.innerHTML= convertSeconds(currentSong.duration);
+    document.querySelector(".circle").style.left=(currentSong.currentTime/currentSong.duration*100)+"%";
+  })
+}
+main();
